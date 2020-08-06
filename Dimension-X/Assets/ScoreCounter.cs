e9bc12542458398576ab369f187214008da5cd7c
Suraj Gupta <48242153+Suraj9300i@users.noreply.github.com>
2020-08-06 11:42:02 +0530

    Unity

---
+++ Dimension-X/Assets/ScoreCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] int ScorePerBlock = 67;
+    int TotalScore = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TotalScore += ScorePerBlock;
+        text.text = TotalScore.ToString();
+    }
+
+    public int getTotalScore()
+    {
+        return TotalScore;
+    }
+}
